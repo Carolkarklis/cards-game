b0cdb4e89c3a6fc0359d53aa603978c8ecdfe492
Carolina Karklis <carolinakarklis@gmail.com>
2018-02-06 18:29:12 -0200

    base of elixir cards app

---
+++ lib/cards.ex
@@ -0,0 +1,39 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  def create_deck do 
+    values = ["Ace", "Two", "Three"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    
+    for value <- values, suit <- suits do 
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do 
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do 
+    { hand, rest_of_deck } = Enum.split(deck, hand_size)
+    hand
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do 
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+end
